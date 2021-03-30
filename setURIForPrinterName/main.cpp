db124a34aa4ee7bf236741025d3365658a265651
Danil Korotenko <danil.korotenko@gmail.com>
2021-03-30 19:03:06 +0300

    setURIForPrinterName initial commit.

---
+++ setURIForPrinterName/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  setURIForPrinterName
+//
+//  Created by Danil Korotenko on 3/30/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
