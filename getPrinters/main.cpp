2c73cc6d2d91e6c135706d88ee0b4d5c93000419
Danil Korotenko <danil.korotenko@gmail.com>
2021-03-30 14:12:05 +0300

    getPrinters initial commit.

---
+++ getPrinters/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  getPrinters
+//
+//  Created by Danil Korotenko on 3/30/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
