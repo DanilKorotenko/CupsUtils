7b2679f04f3e1b4ec49f830486eb32a227c99537
Danil Korotenko <danil.korotenko@gmail.com>
2021-03-30 17:40:18 +0300

    getURIForPrinterName initial commit.

---
+++ getURIForPrinterName/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  getURIForPrinterName
+//
+//  Created by Danil Korotenko on 3/30/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
