6364f10a95086c894c9bb10ee750264265bd0ac1
Danil Korotenko <danil.korotenko@gmail.com>
2021-04-06 15:38:09 +0300

    ppdc sources were added.

--- PPDUtils/PPDUtilsImpl.cpp
+++ PPDUtils/PPDUtilsImpl.cpp
@@ -7,6 +7,11 @@
 
 #include "PPDUtilsImpl.hpp"
 #include <iostream>
+#include <cups/ppd.h>
+
+//#include "ppdc/ppdc.h"
+
+const char *kPPDFilesPath = "/etc/cups/ppd";
 
 PPDUtilsImpl::PPDUtilsImpl()
 {
@@ -23,7 +28,16 @@ PPDUtilsImpl::~PPDUtilsImpl()
 std::string PPDUtilsImpl::getPPDFilePathForPrinterName(
     const std::string &aPrinterName)
 {
-    std::string result;
+    std::string ppdFilePath2 = kPPDFilesPath;
+    ppdFilePath2 += "/";
+    ppdFilePath2 += aPrinterName;
+    ppdFilePath2 += ".ppd";
+
+    return ppdFilePath2;
+
+    const char *ppdFilePath = cupsGetPPD(aPrinterName.c_str());
+//    const char *ppdFilePath = cupsGetServerPPD(CUPS_HTTP_DEFAULT, aPrinterName.c_str());
+    std::string result(ppdFilePath);
     return result;
 }
 
