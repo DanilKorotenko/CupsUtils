989be59e5e90451f98d6580945966020b74d2fa4
Danil Korotenko <danil.korotenko@gmail.com>
2021-04-12 12:19:55 +0300

    CupsUtilsNotificator initial commit.

---
+++ cupsUtilsNotificator/cupsUtilsNotificatorMain.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cupsUtilsNotificator
+//
+//  Created by Danil Korotenko on 4/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
