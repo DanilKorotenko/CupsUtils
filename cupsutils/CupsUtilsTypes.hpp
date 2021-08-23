34290b60c8aa390824d8342a2b34fe04a269cec0
Danil Korotenko <danil.korotenko@gmail.com>
2021-08-23 11:43:17 +0300

    Cups job class moved.

--- cupsutils/CupsUtilsTypes.hpp
+++ cupsutils/CupsUtilsTypes.hpp
@@ -9,6 +9,8 @@
 
 #include <string>
 #include <sstream>
+#include <memory>
+#include <cups/cups.h>
 
 namespace CupsUtilities
 {
@@ -21,20 +23,98 @@ typedef struct
 
 struct CupsPrinter
 {
-//    enum class State
-//    {
-//        Idle = 3,
-//        Processing,
-//        Stopped
-//    };
+    using PtrT = std::shared_ptr<CupsPrinter>;
 
-    std::string name;
-    std::string printerInfo;
-//    std::string uri;
-//    State       state;
-    std::string stateReasons;
+    CupsPrinter() = default;
+    CupsPrinter(const CupsPrinter&) = default;
+    CupsPrinter(CupsPrinter&&) = default;
+
+    CupsPrinter& operator=(const CupsPrinter&) = default;
+    CupsPrinter& operator=(CupsPrinter&&) = default;
+
+    std::string toString() const
+    {
+        std::stringstream result;
+        result << "Printer name: "    << name << std::endl
+               << "URI: "             << uri << std::endl
+               << "Info: "            << printerInfo << std::endl
+               << "Policy: "          << policy << std::endl
+               << "Jobs hold until: " << jobsHoldUntil << std::endl
+               << "State reasons: "   << stateReasons <<  std::endl;
+        return result.str();
+    }
+
+    bool isModified(const CupsPrinter& printer) const
+    {
+        if (uri != printer.uri)
+        {
+            return false;
+        }
+
+        bool result = policy == printer.policy && jobsHoldUntil == printer.jobsHoldUntil;
+        return !result;
+    }
+
+    bool isStateChanged(const CupsPrinter& printer) const
+    {
+        if (uri != printer.uri)
+        {
+            return false;
+        }
+
+        return stateReasons != printer.stateReasons;
+    }
+
+    std::string  name;
+    std::string  printerInfo;
+    std::string  policy;
+    std::string  jobsHoldUntil;
+    std::string  uri;
+    std::string  stateReasons;
+    bool         newPrinter = false; // flag to skip modification event triggered by setup DLP.
 };
 
-}
+class CupsJob
+{
+public:
+    using PtrT = std::shared_ptr<CupsJob>;
+
+    CupsJob()
+            : jobId(0)
+            , title("untitled")
+            , printer()
+            , userName("unknown")
+            , format("application/octet-stream")
+            , size(0)
+            , state(IPP_JOB_PENDING)
+    {
+    }
+
+    std::string toString() const
+    {
+        std::stringstream result;
+
+        result << "Job id: " << jobId << std::endl
+               << "Job title: " << title << std::endl
+               << "Printer name: " << printer.name << std::endl
+               << "Printer info: " << printer.printerInfo << std::endl
+               << "Printer uri: " << printer.uri << std::endl
+               << "Printer state reasons: " << printer.stateReasons << std::endl
+               << "User: " << userName << std::endl
+               << "Format: " << format << std::endl
+               << "Size: " << size << std::endl
+               << "State: " << state << std::endl;
+
+        return result.str();
+    }
+
+    int          jobId;
+    std::string  title;
+    CupsPrinter  printer;
+    std::string  userName;
+    std::string  format;
+    int          size; // Size in kilobytes
+    ipp_jstate_e state;
+};
 
 } // namespace CupsUtilities
