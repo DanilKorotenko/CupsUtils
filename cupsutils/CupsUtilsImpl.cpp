0a36544f63f124016a618179ebc8b180c4f0934f
Danil Korotenko <danil.korotenko@gmail.com>
2021-08-23 12:35:58 +0300

    update

--- cupsutils/CupsUtilsImpl.cpp
+++ cupsutils/CupsUtilsImpl.cpp
@@ -184,6 +184,31 @@ CupsPrinter CupsUtilsImpl::getPrinterWithName(std::string aPrinterName)
     return getPrinterWithDestination(printerDestination);
 }
 
+std::string getPrinterPolicy(cups_dest_t *printerDestination)
+{
+    std::string retValue;
+    std::string printerURI = cupsGetOption(kOptionNamePrinterURI, printerDestination->num_options,
+                                           printerDestination->options);
+
+    ipp_t* request = ::ippNewRequest(IPP_GET_PRINTER_ATTRIBUTES);
+    ::ippAddString(request, IPP_TAG_OPERATION, IPP_TAG_URI, "printer-uri", nullptr, printerURI.c_str());
+    ::ippAddString(request, IPP_TAG_OPERATION, IPP_TAG_NAME, "requesting-user-name", nullptr, cupsUser());
+    ipp_t* response = ::cupsDoRequest(CUPS_HTTP_DEFAULT, request, "/admin/");
+
+    ipp_status_t lastError = cupsLastError();
+    if (lastError == IPP_OK)
+    {
+        ipp_attribute_t *attr = ippFindAttribute(response, "printer-op-policy", IPP_TAG_ZERO);
+        if (const char* result = ippGetString(attr, 0, nullptr))
+        {
+            retValue.assign(result);
+        }
+    }
+    ::ippDelete(response);
+
+    return retValue;
+}
+
 CupsPrinter CupsUtilsImpl::getPrinterWithDestination(cups_dest_t *printerDestination)
 {
     CupsPrinter printer;
@@ -260,7 +285,7 @@ bool CupsUtilsImpl::setOptionForPrinterWithName(std::string aPrinterName,
         printerDestination->num_options,
         &printerDestination->options);
 
-    printerDestination->num_options = cupsAddOption(anOption.name.c_str(),
+    printerDestination->num_options = ::cupsAddOption(anOption.name.c_str(),
         anOption.value.c_str(),
         printerDestination->num_options, &printerDestination->options);
 
@@ -404,476 +429,26 @@ bool CupsUtilsImpl::setPrinterOptions(
     return true;
 }
 
-int			/* O - 1 on match, 0 otherwise */
-_cups_isupper(int ch)			/* I - Character to test */
 {
-  return (ch >= 'A' && ch <= 'Z');
-}
-
-int			/* O - Converted character */
-_cups_tolower(int ch)			/* I - Character to convert */
-{
-  return (_cups_isupper(ch) ? ch - 'A' + 'a' : ch);
-}
-
-/*
- * '_cups_strcasecmp()' - Do a case-insensitive comparison.
- */
-
-int				/* O - Result of comparison (-1, 0, or 1) */
-_cups_strcasecmp(const char *s,	/* I - First string */
-                 const char *t)	/* I - Second string */
-{
-  while (*s != '\0' && *t != '\0')
-  {
-    if (_cups_tolower(*s) < _cups_tolower(*t))
-      return (-1);
-    else if (_cups_tolower(*s) > _cups_tolower(*t))
-      return (1);
-
-    s ++;
-    t ++;
-  }
-
-  if (*s == '\0' && *t == '\0')
-    return (0);
-  else if (*s != '\0')
-    return (1);
-  else
-    return (-1);
-}
-
-/*
- * 'http_copy_decode()' - Copy and decode a URI.
- */
-
-static const char *			/* O - New source pointer or NULL on error */
-http_copy_decode(char       *dst,	/* O - Destination buffer */
-                 const char *src,	/* I - Source pointer */
-		 int        dstsize,	/* I - Destination size */
-		 const char *term,	/* I - Terminating characters */
-		 int        decode)	/* I - Decode %-encoded values */
-{
-  char	*ptr,				/* Pointer into buffer */
-	*end;				/* End of buffer */
-  int	quoted;				/* Quoted character */
 
 
- /*
-  * Copy the src to the destination until we hit a terminating character
-  * or the end of the string.
-  */
-
-  for (ptr = dst, end = dst + dstsize - 1;
-       *src && (!term || !strchr(term, *src));
-       src ++)
-    if (ptr < end)
     {
-      if (*src == '%' && decode)
-      {
-        if (isxdigit(src[1] & 255) && isxdigit(src[2] & 255))
-	{
-	 /*
-	  * Grab a hex-encoded character...
-	  */
-
-          src ++;
-	  if (isalpha(*src))
-	    quoted = (tolower(*src) - 'a' + 10) << 4;
-	  else
-	    quoted = (*src - '0') << 4;
-
-          src ++;
-	  if (isalpha(*src))
-	    quoted |= tolower(*src) - 'a' + 10;
-	  else
-	    quoted |= *src - '0';
-
-          *ptr++ = (char)quoted;
-	}
-	else
-	{
-	 /*
-	  * Bad hex-encoded character...
-	  */
-
-	  *ptr = '\0';
-	  return (NULL);
-	}
-      }
-      else if ((*src & 255) <= 0x20 || (*src & 255) >= 0x7f)
-      {
-        *ptr = '\0';
-        return (NULL);
-      }
-      else
-	*ptr++ = *src;
     }
 
-  *ptr = '\0';
-
-  return (src);
-}
-
-http_uri_status_t			/* O - Result of separation */
-myHttpSeparateURI(
-    http_uri_coding_t decoding,		/* I - Decoding flags */
-    const char        *uri,		/* I - Universal Resource Identifier */
-    char              *scheme,		/* O - Scheme (http, https, etc.) */
-    int               schemelen,	/* I - Size of scheme buffer */
-    char              *username,	/* O - Username */
-    int               usernamelen,	/* I - Size of username buffer */
-    char              *host,		/* O - Hostname */
-    int               hostlen,		/* I - Size of hostname buffer */
-    int               *port,		/* O - Port number to use */
-    char              *resource,	/* O - Resource/filename */
-    int               resourcelen)	/* I - Size of resource buffer */
-{
-  char			*ptr,		/* Pointer into string... */
-			*end;		/* End of string */
-  const char		*sep;		/* Separator character */
-  http_uri_status_t	status;		/* Result of separation */
-
-
- /*
-  * Initialize everything to blank...
-  */
-
-  if (scheme && schemelen > 0)
-    *scheme = '\0';
-
-  if (username && usernamelen > 0)
-    *username = '\0';
-
-  if (host && hostlen > 0)
-    *host = '\0';
-
-  if (port)
-    *port = 0;
-
-  if (resource && resourcelen > 0)
-    *resource = '\0';
-
- /*
-  * Range check input...
-  */
-
-  if (!uri || !port || !scheme || schemelen <= 0 || !username ||
-      usernamelen <= 0 || !host || hostlen <= 0 || !resource ||
-      resourcelen <= 0)
-    return (HTTP_URI_STATUS_BAD_ARGUMENTS);
-
-  if (!*uri)
-    return (HTTP_URI_STATUS_BAD_URI);
-
- /*
-  * Grab the scheme portion of the URI...
-  */
-
-  status = HTTP_URI_STATUS_OK;
-
-  if (!strncmp(uri, "//", 2))
-  {
-   /*
-    * Workaround for HP IPP client bug...
-    */
-
-    strlcpy(scheme, "ipp", (size_t)schemelen);
-    status = HTTP_URI_STATUS_MISSING_SCHEME;
-  }
-  else if (*uri == '/')
-  {
-   /*
-    * Filename...
-    */
-
-    strlcpy(scheme, "file", (size_t)schemelen);
-    status = HTTP_URI_STATUS_MISSING_SCHEME;
-  }
-  else
-  {
-   /*
-    * Standard URI with scheme...
-    */
-
-    for (ptr = scheme, end = scheme + schemelen - 1;
-         *uri && *uri != ':' && ptr < end;)
-      if (strchr("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                 "abcdefghijklmnopqrstuvwxyz"
-		 "0123456789-+.", *uri) != NULL)
-        *ptr++ = *uri++;
-      else
-        break;
-
-    *ptr = '\0';
-
-    if (*uri != ':' || *scheme == '.' || !*scheme)
     {
-      *scheme = '\0';
-      return (HTTP_URI_STATUS_BAD_SCHEME);
     }
 
-    uri ++;
-  }
-
- /*
-  * Set the default port number...
-  */
-
-  if (!strcmp(scheme, "http"))
-    *port = 80;
-  else if (!strcmp(scheme, "https"))
-    *port = 443;
-  else if (!strcmp(scheme, "ipp") || !strcmp(scheme, "ipps"))
-    *port = 631;
-  else if (!_cups_strcasecmp(scheme, "lpd"))
-    *port = 515;
-  else if (!strcmp(scheme, "socket"))	/* Not yet registered with IANA... */
-    *port = 9100;
-  else if (strcmp(scheme, "file") && strcmp(scheme, "mailto") && strcmp(scheme, "tel"))
-    status = HTTP_URI_STATUS_UNKNOWN_SCHEME;
-
- /*
-  * Now see if we have a hostname...
-  */
-
-  if (!strncmp(uri, "//", 2))
-  {
-   /*
-    * Yes, extract it...
-    */
-
-    uri += 2;
-
-   /*
-    * Grab the username, if any...
-    */
-
-    if ((sep = strpbrk(uri, "@/")) != NULL && *sep == '@')
     {
-     /*
-      * Get a username:password combo...
-      */
-
-      uri = http_copy_decode(username, uri, usernamelen, "@",
-                             decoding & HTTP_URI_CODING_USERNAME);
-
-      if (!uri)
-      {
-        *username = '\0';
-        return (HTTP_URI_STATUS_BAD_USERNAME);
-      }
-
-      uri ++;
     }
 
-   /*
-    * Then the hostname/IP address...
-    */
 
-    if (*uri == '[')
     {
-     /*
-      * Grab IPv6 address...
-      */
-
-      uri ++;
-      if (*uri == 'v')
-      {
-       /*
-        * Skip IPvFuture ("vXXXX.") prefix...
-        */
-
-        uri ++;
-
-        while (isxdigit(*uri & 255))
-          uri ++;
-
-        if (*uri != '.')
-        {
-	  *host = '\0';
-	  return (HTTP_URI_STATUS_BAD_HOSTNAME);
-        }
-
-        uri ++;
-      }
-
-      uri = http_copy_decode(host, uri, hostlen, "]",
-                             decoding & HTTP_URI_CODING_HOSTNAME);
-
-      if (!uri)
-      {
-        *host = '\0';
-        return (HTTP_URI_STATUS_BAD_HOSTNAME);
-      }
-
-     /*
-      * Validate value...
-      */
-
-      if (*uri != ']')
-      {
-        *host = '\0';
-        return (HTTP_URI_STATUS_BAD_HOSTNAME);
-      }
-
-      uri ++;
-
-      for (ptr = host; *ptr; ptr ++)
-        if (*ptr == '+')
-	{
-	 /*
-	  * Convert zone separator to % and stop here...
-	  */
-
-	  *ptr = '%';
-	  break;
-	}
-	else if (*ptr == '%')
-	{
-	 /*
-	  * Stop at zone separator (RFC 6874)
-	  */
-
-	  break;
-	}
-	else if (*ptr != ':' && *ptr != '.' && !isxdigit(*ptr & 255))
-	{
-	  *host = '\0';
-	  return (HTTP_URI_STATUS_BAD_HOSTNAME);
-	}
-    }
-    else
-    {
-     /*
-      * Validate the hostname or IPv4 address first...
-      */
-
-      for (ptr = (char *)uri; *ptr; ptr ++)
-        if (strchr(":?/", *ptr))
-	  break;
-        else if (!strchr("abcdefghijklmnopqrstuvwxyz"	/* unreserved */
-			 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"	/* unreserved */
-			 "0123456789"			/* unreserved */
-	        	 "-._~"				/* unreserved */
-			 "%"				/* pct-encoded */
-			 "!$&'()*+,;="			/* sub-delims */
-			 "\\", *ptr))			/* SMB domain */
-	{
-	  *host = '\0';
-	  return (HTTP_URI_STATUS_BAD_HOSTNAME);
-	}
-
-     /*
-      * Then copy the hostname or IPv4 address to the buffer...
-      */
-
-      uri = http_copy_decode(host, uri, hostlen, ":?/",
-                             decoding & HTTP_URI_CODING_HOSTNAME);
-
-      if (!uri)
-      {
-        *host = '\0';
-        return (HTTP_URI_STATUS_BAD_HOSTNAME);
-      }
     }
 
-   /*
-    * Validate hostname for file scheme - only empty and localhost are
-    * acceptable.
-    */
-
-    if (!strcmp(scheme, "file") && strcmp(host, "localhost") && host[0])
     {
-      *host = '\0';
-      return (HTTP_URI_STATUS_BAD_HOSTNAME);
     }
 
-   /*
-    * See if we have a port number...
-    */
 
-    if (*uri == ':')
-    {
-     /*
-      * Yes, collect the port number...
-      */
-
-      if (!isdigit(uri[1] & 255))
-      {
-        *port = 0;
-        return (HTTP_URI_STATUS_BAD_PORT);
-      }
-
-      *port = (int)strtol(uri + 1, (char **)&uri, 10);
-
-      if (*port <= 0 || *port > 65535)
-      {
-        *port = 0;
-        return (HTTP_URI_STATUS_BAD_PORT);
-      }
-
-      if (*uri != '/' && *uri)
-      {
-        *port = 0;
-        return (HTTP_URI_STATUS_BAD_PORT);
-      }
-    }
-  }
-
- /*
-  * The remaining portion is the resource string...
-  */
-
-  if (*uri == '?' || !*uri)
-  {
-   /*
-    * Hostname but no path...
-    */
-
-    status    = HTTP_URI_STATUS_MISSING_RESOURCE;
-    *resource = '/';
-
-   /*
-    * Copy any query string...
-    */
-
-    if (*uri == '?')
-      uri = http_copy_decode(resource + 1, uri, resourcelen - 1, NULL,
-                             decoding & HTTP_URI_CODING_QUERY);
-    else
-      resource[1] = '\0';
-  }
-  else
-  {
-    uri = http_copy_decode(resource, uri, resourcelen, "?",
-                           decoding & HTTP_URI_CODING_RESOURCE);
-
-    if (uri && *uri == '?')
-    {
-     /*
-      * Concatenate any query string...
-      */
-
-      char *resptr = resource + strlen(resource);
-
-      uri = http_copy_decode(resptr, uri,
-                             resourcelen - (int)(resptr - resource), NULL,
-                             decoding & HTTP_URI_CODING_QUERY);
-    }
-  }
-
-  if (!uri)
-  {
-    *resource = '\0';
-    return (HTTP_URI_STATUS_BAD_RESOURCE);
-  }
-
- /*
-  * Return the URI separation status...
-  */
-
-  return (status);
 }
 
 } // namespace CupsUtilities
